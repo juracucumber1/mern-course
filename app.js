37bc6ecee80c6b4d6169428f9bd18903a28c45bd
Jura <mr.gordien@mail.ru>
2022-01-10 07:07:10 +0300

    add-mongo

--- app.js
+++ app.js
@@ -4,18 +4,19 @@ const mogoose = require('mongoose')
 
 const app = express()
 
-const PORT = config.get('port') || 5000
+app.use('/api/auth', require('./routes/auth.routes'))
 
+const PORT = config.get('port') || 5000
 
 async function start() {
     try {
         await mogoose.connect(config.get('mongoUri'), {
             useNewUrlParser:true,
             useUnifiedTopology: true,
-            useCreateIndex: true
+
 
         })
-        app.listen(5000, (PORT,) => console.log(`App hes beet started on port ${PORT}...`))
+        app.listen(PORT, () => console.log(`App hes beet started on port ${PORT}...`))
     } catch (e) {
         console.log('Server Error', e.message)
         process.exit(1)
