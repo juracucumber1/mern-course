26ac3ccca6c1befff7287bd47376f0375f22420a
Jura <mr.gordien@mail.ru>
2022-01-17 20:25:16 +0300

    Improvements-in-the-script

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const config = require('config')
+const patch = require('path')
 const mongoose = require('mongoose')
 
 const app = express()
@@ -9,6 +10,16 @@ app.use(express.json({ extended: true }))
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/link', require('./routes/link.routes'))
 app.use('/t', require('./routes/redirect.routes'))
+
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static(patch.join(__dirname, 'client', 'build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(patch.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+
+}
+
 
 const PORT = config.get('port') || 5000
 
